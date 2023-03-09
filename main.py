f62c4a5a5f8076a1e02c70e291915ab49ac6f2e3
mathias <selandermathias00@gmail.com>
2023-03-09 15:12:54 +0100

    pis machine

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ while True:
 
     duck.move(dinmor, dt, screen)
     first_map.move(duck.pos[0], duck.pos[1], screen)
-    duck.adjust(first_map.colCheck(duck.rect, screen))
+    duck.adjust(first_map.colCheck(duck, screen))
 
     screen.fill((0, 0, 0))
     first_map.blit(screen)
--- obj.py
+++ obj.py
@@ -54,9 +54,10 @@ class sprite:
         w = self.image.get_width() * (size / self.image.get_height())
         self.image = pygame.transform.scale(self.image, (w,h))
 
-    def adjust(self, dist: int):
-        if dist < 0:
-            self.pos[0] += dist
+    def adjust(self, dist: list[int]):
+        print(dist[1] - self.pos[0] - self.rect.width)
+        self.pos[0] += dist[0] - self.pos[0]
+        # self.pos[0] += dist[1] - self.pos[0] - self.rect.width
 
 class Map:
     def __init__(self, path: str, col: str, screen):
@@ -64,6 +65,7 @@ class Map:
         self.colImg = pygame.image.load(col)
         self.collider = pygame.Surface((screen.get_width(), screen.get_height()))
         self.rect = self.image.get_rect()
+        self.colX = [0,0,0,0]
 
     def center(self, screen):
         self.rect.x = -(self.rect.w / 2 - screen.get_width() / 2)
@@ -74,15 +76,29 @@ class Map:
         self.rect.y = y - (self.rect.h / 2 - screen.get_height() / 2)
 
     def blit(self, screen):
-        screen.blit(self.colImg, self.rect)
+        screen.blit(self.image, self.rect)
 
-    def colCheck(self, player: pygame.Rect, screen: pygame.Surface):
+    def colCheck(self, player: sprite, screen: pygame.Surface):
         self.collider.blit(self.colImg, self.rect)
-        if np.mean(self.collider.get_at((player.x, player.y))[:3]) < 10:
-            print("collision")
-       #  x = player.x + round(player.width / 2)
-       #  y = player.y
-       #  while np.mean(self.collider.get_at((x, y))[:3]) > 10 and x > 0:
-       #      x -= 1
-       #  return player.x - x
-        return 1
+        checkPos = [
+            {"x": player.rect.x, "y": player.rect.y, "pos": player.pos[0]},
+            {"x": player.rect.x + player.rect.width, "y": player.rect.y, "pos": player.pos[0] + player.rect.width},
+        ]
+        for index, pos in enumerate(checkPos):
+            if self.__checkAt(pos["x"], pos["y"]):
+                continue
+            else:
+                self.colX[index] = pos["pos"]
+        dist = [self.colX[0] if self.__checkAt(x["x"], x["y"]) else x["pos"] for x in checkPos]
+        return dist
+       #  if self.__checkAt(player.rect.x, player.rect.y):
+       #      return self.colX
+       #  else:
+       #      self.colX = player.pos[0]
+       #  return player.pos[0]
+
+    def __checkAt(self, x: int, y: int):
+        if np.mean(self.collider.get_at((x, y))[:3]) < 10:
+            return True
+        else:
+            return False
